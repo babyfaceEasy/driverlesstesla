0d7a8b26779bb16bcf8417a50e92a055149fffd7
leCodeManiac <oodegbaro@gmail.com>
2021-03-15 13:48:18 +0100

    testing templates

--- start.py
+++ start.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello there!"
+	returnrender_template("index.html")
+
+@app.route("/profile")
+def profile():
+	render_template("profile.thml")
+
+@app.route("/login")
+def login():
+	return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+	return render_template("signup.html")
+
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0')
